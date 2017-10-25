14565c8c8852210d46bc3db58b89b76539a1a4a2
Darryl Zeigler <Dzeig.j@gmail.com>
2017-10-25 15:10:19 -0400

    hannnnnn

---
+++ src/components/About/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <div>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/g-pwilkUUto" frameBorder="0" allowFullScreen></iframe>
+    <p>Include your about me here!</p>
+  </div>
+)
+
+export default About;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1>Welcome To NERP-WebpackV3 with React Router</h1>
+    <a href='#'>Learn more about Webpack</a> <br /><br />
+    <a href='#'>Learn more about React Router</a>
+  </div>
+)
+
+export default Home;
